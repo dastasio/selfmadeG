3521349ff6e78ebae57db103c6dcf3508c6ed89a
DESKTOP-E2DFPA3\david <davide.stasio@gmail.com>
2019-03-23 22:01:04 +0100

    0.0.1b Indexed rendering

--- code/win32_selfmadex.cpp
+++ code/win32_selfmadex.cpp
@@ -16,6 +16,8 @@
 #include <cstdio>
 
 #define VSYNC(x) SDL_GL_SetSwapInterval(x)
+#define VERTEX_BUFFER 0
+#define INDEX_BUFFER  1
 #define WIDTH 1024
 #define HEIGHT 720
 
@@ -84,20 +86,29 @@ main()
                 glDeleteShader(Fshader);
                 
 
-                GLfloat vertexData[] =
+                GLfloat VertexData[] =
                 {
                     -0.5f, -0.5f,
                      0.5f, -0.5f,
-                     0.0f,  0.5f
+                     0.5f,  0.5f,
+                    -0.5f,  0.5f
+                };
+                GLuint IndexData[] =
+                {
+                    0, 1, 2,
+                    2, 3, 0
                 };
 
-                GLuint VBO, VAO;
+                GLuint BO[2], VAO;
                 glGenVertexArrays(1, &VAO);
                 glBindVertexArray(VAO);
                 
-                glGenBuffers(1, &VBO);
-                glBindBuffer(GL_ARRAY_BUFFER, VBO);
-                glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+                glGenBuffers(2, BO);
+                glBindBuffer(GL_ARRAY_BUFFER, BO[VERTEX_BUFFER]);
+                glBufferData(GL_ARRAY_BUFFER, sizeof(VertexData), VertexData, GL_STATIC_DRAW);
+
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, BO[INDEX_BUFFER]);
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(IndexData), IndexData, GL_STATIC_DRAW);
 
                 glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), BUFFER_OFFSET(0));
                 glEnableVertexAttribArray(0);
@@ -119,7 +130,7 @@ main()
                     }
 
                     glClear(GL_COLOR_BUFFER_BIT);
-                    glDrawArrays(GL_TRIANGLES, 0, 3);
+                    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
                     SDL_GL_SwapWindow(Window);
 
                     currentTime = SDL_GetTicks();
